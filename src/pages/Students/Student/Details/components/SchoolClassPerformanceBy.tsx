aef4890495cb56cd85a247747344c83f052ddc09
bea <beatriztattooer@gmail.com>
2023-08-28 15:52:40 -0300

    feat[student-details]: schoolClass card

--- src/pages/Students/Student/Details/components/SchoolClassPerformanceBy.tsx
+++ src/pages/Students/Student/Details/components/SchoolClassPerformanceBy.tsx
@@ -1,6 +1,7 @@
+// Components:
 import { Accordion, Box, Flex, Select, Stack, Text, useMantineTheme } from "@mantine/core";
 
-// Chart:
+// Charts:
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -12,14 +13,19 @@ import {
 } from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 import { useState } from "react";
-import { monthsAbbreviation } from "~/constants";
 
 // Stars:
 import { Rating } from '@smastrom/react-rating'
 import '@smastrom/react-rating/style.css'
+import { useGetExamsCharts, useGetPlanetsCharts } from "~/api/school-class";
+import { errorNotification } from "~/utils/errorNotification";
 
-export function SchoolClassPerformanceBy() {
+type componentProps = {
+    schoolClassId: string;
+}
+export function SchoolClassPerformanceBy({ schoolClassId }: componentProps) {
     const theme = useMantineTheme();
+
     // Graphic stuff:
     ChartJS.register(
         CategoryScale,
@@ -31,6 +37,7 @@ export function SchoolClassPerformanceBy() {
     );
 
     const options = {
+        aspectRatio: 4,
         responsive: true,
         interaction: {
             mode: 'index' as const,
@@ -61,172 +68,62 @@ export function SchoolClassPerformanceBy() {
             },
         },
     };
-    // TODO: get real data when backend is finished
-    const PhonologicalAwarenessData = [
-        {
-            month: "Jan",
-            testValue: 60,
-        },
-        {
-            month: "Fev",
-            testValue: 74,
-        },
-        {
-            month: "Mar",
-            testValue: 77,
-        },
-        {
-            month: "Abr",
-            testValue: 80,
-        },
-        {
-            month: "Jun",
-            testValue: 73,
-        },
-        {
-            month: "Jul",
-            testValue: 85,
-        },
-        {
-            month: "Ago",
-            testValue: 85,
-        },
-        {
-            month: "Set",
-            testValue: 85,
-        },
-        {
-            month: "Out",
-            testValue: 80,
-        },
-        {
-            month: "Nov",
-            testValue: 87,
-        },
-        {
-            month: "Dez",
-            testValue: 90,
-        },
-    ]
-    const AlphabeticWritingSystem = [
-        {
-            month: "Jan",
-            testValue: 30,
-        },
-        {
-            month: "Fev",
-            testValue: 60,
-        },
-        {
-            month: "Mar",
-            testValue: 70,
-        },
-        {
-            month: "Abr",
-            testValue: 80,
-        },
-        {
-            month: "Jun",
-            testValue: 70,
-        },
-        {
-            month: "Jul",
-            testValue: 80,
-        },
-        {
-            month: "Ago",
-            testValue: 90,
-        },
-        {
-            month: "Set",
-            testValue: 85,
-        },
-        {
-            month: "Out",
-            testValue: 95,
-        },
-        {
-            month: "Nov",
-            testValue: 100,
-        },
-        {
-            month: "Dez",
-            testValue: 100,
-        },
-    ]
-    const TextReadingAndComprehension = [
-        {
-            month: "Jan",
-            testValue: 70,
-        },
-        {
-            month: "Fev",
-            testValue: 70,
-        },
-        {
-            month: "Mar",
-            testValue: 75,
-        },
-        {
-            month: "Abr",
-            testValue: 80,
-        },
-        {
-            month: "Jun",
-            testValue: 75,
-        },
-        {
-            month: "Jul",
-            testValue: 85,
-        },
-        {
-            month: "Ago",
-            testValue: 90,
-        },
-        {
-            month: "Set",
-            testValue: 85,
-        },
-        {
-            month: "Out",
-            testValue: 95,
-        },
-        {
-            month: "Nov",
-            testValue: 100,
-        },
-        {
-            month: "Dez",
-            testValue: 100,
-        },
-    ]
 
-    const [testsData, setTestsData] = useState({
-        labels: monthsAbbreviation,
-        datasets: [
-            {
-                label: "Consciência Fonológica",
-                data: PhonologicalAwarenessData.map((data) => data.testValue),
-                borderColor: theme.colors.cyan[3],
-                backgroundColor: theme.colors.cyan[3],
-                yAxisID: 'y',
-            },
-            {
-                label: "Sistema de Escrita Alfabética",
-                data: AlphabeticWritingSystem.map((data) => data.testValue),
-                borderColor: theme.colors.violet[2],
-                backgroundColor: theme.colors.violet[2],
-                yAxisID: 'y',
-            },
-            {
-                label: "Leitura e Compreensão de Texto",
-                data: TextReadingAndComprehension.map((data) => data.testValue),
-                borderColor: theme.colors.orange[3],
-                backgroundColor: theme.colors.orange[3],
-                yAxisID: 'y',
-            },
-        ],
+    const { data: schoolClassPerformanceByPlanets } = useGetPlanetsCharts(
+        schoolClassId ?? "",
+        {
+            onError: (error) =>
+                errorNotification("Erro durante a operação", error.message)
+        }
+    )
+
+    const { data: schoolClassPerformanceByExams } = useGetExamsCharts(
+        schoolClassId ?? "",
+        {
+            onError: (error) =>
+                errorNotification("Erro durante a operação", error.message)
+        }
+    )
+
+    schoolClassPerformanceByPlanets?.datasets.forEach(element => {
+        if (element.label == "Consciência Fonológica") {
+            element.backgroundColor = theme.colors.cyan[3]
+            element.borderColor = theme.colors.cyan[3]
+        } else if (element.label == "Sistema de Escrita Alfabética") {
+            element.backgroundColor = theme.colors.violet[2]
+            element.borderColor = theme.colors.violet[2]
+        } else {
+            element.backgroundColor = theme.colors.orange[3]
+            element.borderColor = theme.colors.orange[3]
+        }
+        element.yAxisID = 'y'
     });
+
+    schoolClassPerformanceByExams?.datasets.forEach(element => {
+        if (element.label == "Consciência Fonológica") {
+            element.backgroundColor = theme.colors.cyan[3]
+            element.borderColor = theme.colors.cyan[3]
+        } else if (element.label == "Sistema de Escrita Alfabética") {
+            element.backgroundColor = theme.colors.violet[2]
+            element.borderColor = theme.colors.violet[2]
+        } else {
+            element.backgroundColor = theme.colors.orange[3]
+            element.borderColor = theme.colors.orange[3]
+        }
+        element.yAxisID = 'y'
+    });
+
+    const [performanceType, setPerformanceType] = useState('');
+    const selectOptions = [
+        {
+            label: 'Provas',
+            value: 'Provas'
+        },
+        {
+            label: 'Planetas',
+            value: 'Planetas'
+        }
+    ]
     return (
         <Accordion.Item value="schoolClassPerformanceBy">
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
@@ -240,12 +137,13 @@ export function SchoolClassPerformanceBy() {
                         <Text pr={10}>Desempenho da turma por</Text>
                         <Select
                             withinPortal
-                            data={[]}
+                            data={selectOptions}
                             placeholder="Pesquisar"
                             searchable
                             style={{
                                 width: '150px'
                             }}
+                            onChange={(value) => setPerformanceType(value)}
                         />
                     </Flex>
                 </Accordion.Control>
@@ -254,14 +152,23 @@ export function SchoolClassPerformanceBy() {
 
             <Accordion.Panel>
                 <Flex>
-                    <Stack w={200}>
-                        <Rating readOnly value={5} key={Math.random()} style={{ width: '150px' }} />
-                        <Rating readOnly value={4} key={Math.random()} style={{ width: '150px' }} />
-                        <Rating readOnly value={3} key={Math.random()} style={{ width: '150px' }} />
-                        <Rating readOnly value={2} key={Math.random()} style={{ width: '150px' }} />
-                        <Rating readOnly value={1} key={Math.random()} style={{ width: '150px' }} />
-                    </Stack>
-                    <Bar options={options} data={testsData} />
+                    {performanceType == "Planetas" &&
+                        <Stack w={200}>
+                            <Rating readOnly value={5} key={Math.random()} style={{ width: '150px' }} />
+                            <Rating readOnly value={4} key={Math.random()} style={{ width: '150px' }} />
+                            <Rating readOnly value={3} key={Math.random()} style={{ width: '150px' }} />
+                            <Rating readOnly value={2} key={Math.random()} style={{ width: '150px' }} />
+                            <Rating readOnly value={1} key={Math.random()} style={{ width: '150px' }} />
+                        </Stack>
+                    }
+
+                    {performanceType == "Provas" &&
+                        <Bar options={options} data={schoolClassPerformanceByExams} />
+                    }
+
+                    {performanceType == "Planetas" &&
+                        <Bar options={options} data={schoolClassPerformanceByPlanets} />
+                    }
                 </Flex>
             </Accordion.Panel>
         </Accordion.Item>
