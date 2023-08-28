0f8a4263253f6de8cde45a7028ce6b957b520ec3
bea <beatriztattooer@gmail.com>
2023-08-28 15:52:53 -0300

    :bug: fix[api]: student

--- src/api/student.ts
+++ src/api/student.ts
@@ -62,6 +62,7 @@ const KEY = {
   PLANETS_CHART_BY_ID: "PLANETS_CHART_BY_ID",
   EXAM_EXECUTIONS: "EXAM_EXECUTIONS",
   EXAMS_PERFORMANCE_PLANETS: "EXAMS_PERFORMANCE_PLANETS",
+  DEITALED_SUMMARY_BY_ID: "DEITALED_SUMMARY_BY_ID",
 };
 
 class StudentAPI extends API {
@@ -232,7 +233,7 @@ export function useAuthorizeNewExam(
 
 export function useGetDetailedSummary(
   id: string,
-  options?: QueryOptions<Student, [typeof KEY.BY_ID, string]>
+  options?: QueryOptions<Student, [typeof KEY.DEITALED_SUMMARY_BY_ID, string]>
 ) {
   const handler = useCallback(
     function () {
@@ -240,7 +241,7 @@ export function useGetDetailedSummary(
     },
     [id]
   )
-  return useQuery([KEY.BY_ID, id], handler, options)
+  return useQuery([KEY.DEITALED_SUMMARY_BY_ID, id], handler, options)
 }
 
 export function useGetExamCharts(
