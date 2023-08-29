f35b7cd49f2fb69230214677300a31d91e4edd0e
bea <beatriztattooer@gmail.com>
2023-08-29 16:05:06 -0300

    :sparkles: feat[schoolClass-details]: exams chart

---
+++ src/pages/Classes/Detail/components/ExamsChart/Chart.tsx
@@ -0,0 +1,72 @@
+import { monthsAbbreviation } from "~/constants";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend,
+} from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import { useMantineTheme } from "@mantine/core";
+import { useState } from "react";
+
+type componentProps = {
+    schoolClassExamsChart: Array<[]>
+}
+export function Chart({ schoolClassExamsChart }: componentProps) {
+    const theme = useMantineTheme()
+
+    // Graphic stuff:
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Tooltip,
+        Legend
+    );
+
+    const options = {
+        aspectRatio: 4,
+        responsive: true,
+        interaction: {
+            mode: 'index' as const,
+            intersect: false,
+        },
+        stacked: false,
+        plugins: {
+            title: {
+                display: false,
+            },
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            y: {
+                type: 'linear' as const,
+                display: true,
+                position: 'left' as const,
+            },
+            y1: {
+                type: 'linear' as const,
+                display: true,
+                position: 'right' as const,
+                grid: {
+                    drawOnChartArea: false,
+                },
+            },
+        },
+    };
+
+    const [testsData, setTestsData] = useState({
+        labels: schoolClassExamsChart?.labels ?? [],
+        datasets: schoolClassExamsChart?.datasets ?? []
+    });
+
+    return (
+        <Line options={options} data={testsData} />
+    )
+}
