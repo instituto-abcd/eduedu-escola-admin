770ba4e8ca63661379ef63531bcd36a0b57aca72
Guilherme Tabuada <ggtbh@outlook.com>
2023-12-04 15:25:31 -0300

    Change queryKey

--- src/api/sync.ts
+++ src/api/sync.ts
@@ -3,20 +3,6 @@ import { API } from "./base";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { MutationOptions, QueryOptions } from "./api-types";
 
-// type Settings = {
-//   id: string;
-//   schoolName?: string;
-//   synchronizationPlanets: boolean;
-//   smtpHostName: string;
-//   smtpUserName: string;
-//   smtpPassword: string;
-//   smtpPort: string;
-//   sslIsActive: boolean;
-//   schoolId: string;
-//   createdAt: string;
-//   updatedAt: string;
-// };
-
 type SyncStatus = {
   totalFiles: number;
   syncedFiles: number;
@@ -78,5 +64,5 @@ export function useSyncStatus(
     return SyncAPI.getSyncStatus();
   }, []);
 
-  return useQuery([KEY.PLANETS], handler, options);
+  return useQuery([KEY.SYNCSTATUS], handler, options);
 }
