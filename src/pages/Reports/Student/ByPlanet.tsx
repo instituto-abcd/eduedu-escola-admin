aa48ce7e9710f59f267df0eadf9dad925ad5c3ec
Willian <willian.s@lyncas.net>
2023-12-28 16:32:42 -0300

    fix(reports): get data to plot planet performance by planets

--- src/pages/Reports/Student/ByPlanet.tsx
+++ src/pages/Reports/Student/ByPlanet.tsx
@@ -17,31 +17,22 @@ import {
     Legend,
 } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
-import { useEffect, useState } from "react";
-;
 
 type componentProps = {
     studentId: string;
     maxWidth?: string;
 }
+
 export function ByPlanet({ studentId, maxWidth }: componentProps) {
     const theme = useMantineTheme();
-    const [studentPerformanceByPlanets, setStudentPerformanceByPlanets] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                // const data = await useGetPlanetsCharts(studentId);
-                // setStudentPerformanceByPlanets(data);
-            } catch (error) {
-                errorNotification("Erro durante a operação", error.message);
-            }
-        };
-
-        if (studentId) {
-            fetchData();
+    const { data: studentPerformanceByPlanets } = useGetPlanetsCharts(
+        studentId ?? "",
+        {
+            onError: (error) =>
+                errorNotification("Erro durante a operação", error.message)
         }
-    }, [studentId]);
+    );
 
     if (!studentId) {
         return null; // Renderiza algo diferente ou apenas não renderiza nada enquanto studentId é nulo
@@ -93,7 +84,7 @@ export function ByPlanet({ studentId, maxWidth }: componentProps) {
     const processedPlanetsData = processChartData(studentPerformanceByPlanets?.datasets, theme);
 
     return (
-        <Box mt={40}>
+        <Box mt={10}>
             <Title order={4} pb={20}>Desempenho do aluno por planeta:</Title>
             {processedPlanetsData &&
                 <Line
