3fbe7f299a653e9f87e4a3570fcd02cf92a3a2bc
gtabuada <ggtbh@outlook.com>
2024-07-10 14:48:07 -0300

    Update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex } from "@mantine/core";
+import { Box, Container, Flex, Group } from "@mantine/core";
 import { useUserStore } from "~/stores/user";
 
 const VERSION = import.meta.env.VITE_APP_VERSION;
@@ -6,12 +6,17 @@ const VERSION = import.meta.env.VITE_APP_VERSION;
 export function Footer() {
   const { schoolName } = useUserStore();
   return (
-    <Box style={{ backgroundColor: '#509BCA', color: '#fff', padding: '12px 0' }}>
+    <Box
+      style={{ backgroundColor: "#509BCA", color: "#fff", padding: "12px 0" }}
+    >
       <Container>
         <Flex justify="space-between">
           <b>{schoolName}</b>
-          <span>Desenvolvido por EduEdu</span>
-          <span>{VERSION ? `Versão ${VERSION}` : 'v1.0.0'}</span>
+          <Group spacing={12}>
+            <span>Desenvolvido por EduEdu+</span>
+            &mdash;
+            <span>{VERSION ? `Versão ${VERSION}` : "v1.0.0"}</span>
+          </Group>
         </Flex>
       </Container>
     </Box>
