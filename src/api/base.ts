507817721eb46452f8c42ab938a2937577493d40
gtabuada <ggtbh@outlook.com>
2025-04-28 16:34:59 -0300

    hotfix: port workaround

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,75 +1,75 @@
 import axios, {
-  AxiosError,
-  AxiosInstance,
-  InternalAxiosRequestConfig,
+	AxiosError,
+	AxiosInstance,
+	InternalAxiosRequestConfig,
 } from "axios";
 import { useUserStore } from "../stores/user";
 import { BaseError } from "./api-types";
 
 export class API {
-  static readonly api: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-  });
+	static readonly api: AxiosInstance = axios.create({
+		baseURL: import.meta.env.VITE_API_URL + ":3000",
+	});
 
-  private static readonly tokenInterceptorId =
-    this.api.interceptors.request.use(this.appendToken);
+	private static readonly tokenInterceptorId =
+		this.api.interceptors.request.use(this.appendToken);
 
-  private static readonly _rtiid = this.api.interceptors.response.use(
-    undefined,
-    this.transformError,
-  );
+	private static readonly _rtiid = this.api.interceptors.response.use(
+		undefined,
+		this.transformError,
+	);
 
-  private static appendToken(req: InternalAxiosRequestConfig<unknown>) {
-    const { accessToken } = useUserStore.getState();
+	private static appendToken(req: InternalAxiosRequestConfig<unknown>) {
+		const { accessToken } = useUserStore.getState();
 
-    return {
-      ...req,
-      headers: {
-        ...req.headers,
-        Authorization: `Bearer ${accessToken}`,
-      },
-    } as InternalAxiosRequestConfig<unknown>;
-  }
+		return {
+			...req,
+			headers: {
+				...req.headers,
+				Authorization: `Bearer ${accessToken}`,
+			},
+		} as InternalAxiosRequestConfig<unknown>;
+	}
 
-  private static async transformError(err: AxiosError<BaseError>) {
-    if (err.response) {
-      if (err.response?.status === 401) {
-        return await API.checkToken();
-      }
+	private static async transformError(err: AxiosError<BaseError>) {
+		if (err.response) {
+			if (err.response?.status === 401) {
+				return await API.checkToken();
+			}
 
-      if (err.response?.status === 403) {
-        // return showErrorNotification(
-        //   ["Você não tem permissão para acessar este recurso"],
-        //   "Acesso negado"
-        // );
-      }
+			if (err.response?.status === 403) {
+				// return showErrorNotification(
+				//   ["Você não tem permissão para acessar este recurso"],
+				//   "Acesso negado"
+				// );
+			}
 
-      if (err.response?.status === 500) {
-        // return showErrorNotification(
-        //   ["Por favor contate um administrador"],
-        //   "Erro interno"
-        // );
-      }
+			if (err.response?.status === 500) {
+				// return showErrorNotification(
+				//   ["Por favor contate um administrador"],
+				//   "Erro interno"
+				// );
+			}
 
-      // showErrorNotification(err.response.data.errors);
+			// showErrorNotification(err.response.data.errors);
 
-      throw err.response.data;
-    } else throw err;
-  }
+			throw err.response.data;
+		} else throw err;
+	}
 
-  private static async checkToken() {
-    try {
-      const { accessToken } = useUserStore.getState();
-      if (!accessToken) throw Error();
+	private static async checkToken() {
+		try {
+			const { accessToken } = useUserStore.getState();
+			if (!accessToken) throw Error();
 
-      const { data } = await this.api.get<{ valido: boolean }>(
-        "login/token-valido",
-      );
+			const { data } = await this.api.get<{ valido: boolean }>(
+				"login/token-valido",
+			);
 
-      if (!data.valido) throw Error();
-    } catch {
-      useUserStore.setState({}, true);
-      window.location.pathname = "/login";
-    }
-  }
+			if (!data.valido) throw Error();
+		} catch {
+			useUserStore.setState({}, true);
+			window.location.pathname = "/login";
+		}
+	}
 }
