423d4aba02e63b5e354db6b4e113a2d6d24da4ef
BacchoTerra <brunobacchot@hotmail.com>
2025-11-18 15:05:23 -0300

    Correção de nome de variavel em entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@
 CONFIG_FILE_PATH=/usr/share/nginx/html/config.js
 
 echo "Gerando arquivo de configuração em ${CONFIG_FILE_PATH}..."
+echo "Config: APP_URL: ${APP_URL}"
+echo "Config: APP_VERSION: ${APP_VERSION}"
 
 cat <<EOF > ${CONFIG_FILE_PATH}
 window.config = {
-  API_URL: "${API_URL}",
+  API_URL: "${APP_URL}",
     APP_VERSION: "${APP_VERSION}"
 };
 EOF
